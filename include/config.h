41a8d436d3803d71e08e844b4b9cec077e5d91ff
Herr Cult <10423041@student.vgu.edu.vn>
2025-05-19 16:12:17 +0700

    Added the feature to save the MAC of the receiver, force re-pairing.

---
+++ include/config.h
@@ -0,0 +1,10 @@
+	#include <Arduino.h>
+	
+	#define NODE_ID "S-0"
+	#define DHT_PIN 33
+	#define DHT_TYPE DHT11
+	#define MOISTURE_PIN 32
+	#define SLEEP_TIMER 10
+	#define AIR_VALUE 2559
+	#define WATER_VALUE 1225
+	#define DEFAULT_MEASUREMENT -100
